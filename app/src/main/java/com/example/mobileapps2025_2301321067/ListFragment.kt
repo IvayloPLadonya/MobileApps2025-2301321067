e4024cfc397f1e994281b3b792a01955eb6779ab
IvayloPLadonya <ivailoladonia@gmail.com>
2025-11-23 13:32:50 +0200

    Updated List Fragment

--- app/src/main/java/com/example/mobileapps2025_2301321067/ListFragment.kt
+++ app/src/main/java/com/example/mobileapps2025_2301321067/ListFragment.kt
@@ -5,12 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnAdd = view.findViewById<FloatingActionButton>(R.id.btnAdd)
+
+        btnAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addExerciseFragment)
+        }
     }
 }
